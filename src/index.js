75f5c5abfd48ddd3472964ecc5c6615d118712d6
Mike Farrow <contact@mikefarrow.co.uk>
2017-03-01 19:35:35 +0000

    added logging

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 var express = require('express')
+var morgan = require('morgan')
+
 var app = express()
+var development = process.env.NODE_ENV === 'development'
 
 app.set('view engine', 'pug')
 app.set('views', './templates/views')
 
 app.use(express.static('public'))
+app.use(morgan(development ? 'dev' : 'common'))
 
 require('./routes')(app)
 require('./utils/watch')
