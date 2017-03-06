83c078e98762edddd9532f7c0df1603259b6de5d
Mike Farrow <contact@mikefarrow.co.uk>
2017-03-06 15:25:15 +0000

    accounting for proxy in request logging

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 var express = require('express')
 var morgan = require('morgan')
 
+morgan.token('remote-addr', function (req, res) {
+  return req.headers['x-forwarded-for'] || req.ip
+})
+
 var app = express()
 var development = process.env.NODE_ENV === 'development'
 
