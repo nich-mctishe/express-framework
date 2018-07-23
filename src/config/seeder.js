73b984eac15d56ec77efd9d454793c9ef75c977d
Nicholas Headlong <nicholas.headlong@aesopagency.com>
2018-07-23 18:10:56 +0100

    swapped out seeder for package

--- src/config/seeder.js
+++ src/config/seeder.js
@@ -1,6 +1,6 @@
 // this file can be set up to run the seeder and await for the end.
 // this will then fire as a .then
-const seeder = require('../seeder')
+const seeder = require('node-seeder')
 const path = require('path')
 const location = path.join(__dirname, '/../seeder/updates/')
 const models = require('../helpers/models')
--- src/seeder/models/updates.js
+++
@@ -1,8 +0,0 @@
-const Mongoose = require('mongoose')
-const Schema = Mongoose.Schema
-
-module.exports = Mongoose.model('updates', new Schema({
-  key: { type: String, required: true }
-}, {
-  timestamps: true
-}))
